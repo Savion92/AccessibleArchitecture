ecc1f81d1be4ac137cbd630df31cd8dfa1173982
Savion <savion90@gmail.com>
2025-04-18 18:31:38 -0400

    add: base backend, DB, vite, node modules

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,18 @@
+export const signup = async (req, res) => {
+  res.json({
+    data: "You're at signup",
+  });
+};
+
+
+export const login = async (req, res) => {
+  res.json({
+    data: "You're at login",
+  });
+};
+
+export const logout = async (req, res) => {
+  res.json({
+    data: "You're at logout",
+  });
+};
