8d9d215d65faad3cfa1590abc36472c596df6c1a
Savion <savion90@gmail.com>
2025-04-18 20:01:08 -0400

    add: user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    websites: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Website",
+        default: []
+      },
+    ],
+  },
+  { timestamps: true }
+);
