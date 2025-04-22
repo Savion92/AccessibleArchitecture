e700caa5f6d4147e08829ad617034525e7746289
Savion <savion90@gmail.com>
2025-04-22 17:34:28 -0400

    add: deployment scripts

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,14 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+const __dirname = path.resolve();
 
 app.use(express.json()); // middleware: parses req.json
 app.use(express.urlencoded({ extended: true }));
@@ -16,6 +19,13 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectMongoDB();
